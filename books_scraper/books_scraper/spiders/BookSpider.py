1430275f279ab6c989142d38a87627296a4d8044
Rangon Roy Utsab <rangonroy@outlook.com>
2026-06-25 19:20:52 +0600

    .

--- books_scraper/books_scraper/spiders/BookSpider.py
+++ books_scraper/books_scraper/spiders/BookSpider.py
@@ -2,22 +2,18 @@ from pathlib import Path
 
 import scrapy
 
-ALLOWED_DOMAINS = ["books.toscrape.com"]
-START_URLS = ["https://books.toscrape.com/index.html"]
-
 
 class BookSpider(scrapy.Spider):
     name = "book"
 
-    # allowed_domains = ALLOWED_DOMAINS
-    # start_urls = START_URLS
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/index.html"]
 
     async def start(self):
-        for url in START_URLS:
+        for url in start_urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
-        Path(filename).write_bytes(response.body)
-        self.log(f"Saved file {filename}")
+        categories = response.css("ul.nav.nav-list > li > ul > li").getall()
+
+        
